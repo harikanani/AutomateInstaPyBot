a6b800dc3501873fea0240183b09fae44ad026bc
Akash20x <46225357+Akash20x@users.noreply.github.com>
2021-02-22 01:54:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pyautogui
 class InstaBot:
     def __init__(self,username,pwd):
         # CHANGE THE LOCATION OF CHROMEDRIVER BELOW #
-        self.driver=webdriver.Chrome("D:\chromedriver\chromedriver.exe")
+        self.driver=webdriver.Chrome("D:\chromedriver\chromedriver.exe")        
         self.username=username
 
         self.driver.get("https://instagram.com")
@@ -21,9 +21,8 @@ class InstaBot:
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
             .click()
         sleep(1)
-        
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
-           .click()
+            .click()
         sleep(2)
     
 
@@ -36,39 +35,27 @@ class InstaBot:
         self.driver.implicitly_wait(3)
         name=self.driver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/div[3]/div/div[2]/div/div[1]/a')
         name.click()
-        self.driver.implicitly_wait(18) 
-        self.like()	
+        
     def like(self):
-        self.driver.implicitly_wait(4)
         sleep(2)
-        start = self.driver.find_element_by_xpath("//*[@id='react-root']/section/main/div/div[3]")
-        
-        a=0
-        self.driver.execute_script('arguments['+str(a)+'].scrollIntoView()',start)
-        
-        for i in range(10):
-            self.driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
-            sleep(3)
-            pics=self.driver.find_elements_by_class_name("v1Nh3")
+        driver=self.driver
+        driver.find_element_by_class_name("v1Nh3").click()  # To select the post
+
+        i=1
+        number=10 # No of posts to be liked
+        like="True"
+        action='Like' if like else 'Unlike'
+        while i<=number:
             sleep(1)
-            for p in pics:
-                if(a==0):
-                    p.click()
-                    self.driver.implicitly_wait(4)
-                like=self.driver.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button")
-                like.click()
-                sleep(2)
-                if(a==0):
-                    self.driver.find_element_by_xpath("/html/body/div[4]/div[1]/div/div/a").click()
-                else:
-                    self.driver.find_element_by_xpath("/html/body/div[4]/div[1]/div/div/a[2]").click()
-                a+=1
-                print(a)
-
-
+            driver.find_element_by_class_name("fr66n").click() #like a posts
+            sleep(1)
+            driver.find_element_by_xpath("//*[@aria-label='{}']".format(action)).click() #To check post like or unlike 
+            driver.find_element_by_class_name("coreSpriteRightPaginationArrow").click() #Go to next post
+            sleep(1)
+            i=i+1 
+        
     def autoliker(self,num):
-        self.driver.find_element_by_xpath("//*[@id='react-root']/section/nav/div[2]/div/div/div[3]/div/div[1]/div/a")\
-        .click()
+        self.driver.get("https://www.instagram.com/")
         sleep(3)
         pyautogui.moveTo(625,657)
         for i in range(num):
@@ -78,9 +65,8 @@ class InstaBot:
             pyautogui.scroll(-800) 
             sleep(1)
     
-my_bot = InstaBot('username','password')
+my_bot = InstaBot('coder.602','adventure')
 my_bot.go_home()
 my_bot.like()
 num=30 # Likes 15 images
 my_bot.autoliker(num)
-
